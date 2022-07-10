14992bb8feaa836da05a236ff817c93ce265b6ef
benzkittisak <benzkittisak@stv.ac.th>
2022-07-10 18:11:18 +0700

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Kittisak Panluea on 10/7/2565 BE.
+//
+
+import UIKit
+import CoreML
+
+// Vision ช่วยให้เราสามารถประมวลผลรูปภาพเพื่อใช้กับ CoreML โดยที่เราไม่ต้องเขียนโค้ดเยอะ
+import Vision
+/*
+ UIImagePickerControllerDelegate
+ เอามาทำปุ่มที่เป็นรูปกล้อง พอกดไปแล้วมันไปเปิดแอปกล้องมาถ่ายภาพ แล้วก็จะถามว่า
+ จะใช้รูปนี้ไหม หรือว่าจะถ่ายใหม่ดี แบบในแอปที่ชอบให้เราถ่ายบัตรประชาชนอะ
+ */
+
+class ViewController: UIViewController , UIImagePickerControllerDelegate , UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
