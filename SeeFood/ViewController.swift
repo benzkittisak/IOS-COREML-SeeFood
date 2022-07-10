e30ebb7bffda76aa840d8ae3e986df0d795f928f
benzkittisak <benzkittisak@stv.ac.th>
2022-07-10 18:40:49 +0700

    อัปเดต ให้สามารถเรียกใช้งานกล้องของโทรศัพท์ได้แล้ว

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -5,6 +5,14 @@
 //  Created by Kittisak Panluea on 10/7/2565 BE.
 //
 
+/*
+  คือมันจะพังเรื่อง NSCameraUsageDescription เพราะว่าเราไม่ได้อธิบายเอาไว้ว่าเราใช้กล้องเพราะอะไร
+ ดังนั้นเราต้องไปเขียนใน info.plist ก่อน ก็คือไปเพิ่มตัว Privacy เรื่องกล้องนั่นแหละ
+ เพิ่ม
+ Privacy - Photo Library Usage Description
+ Privacy - Camera Usage Description
+ */
+
 import UIKit
 import CoreML
 
@@ -20,11 +28,48 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate , UINav
 
     @IBOutlet weak var imageView: UIImageView!
     
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        
+//        มันจะอนุญาตให้เราสามารถเชื่อมต่อเข้ากลับกล้องได้
+        imagePicker.sourceType = .camera
+//        ถ้าต้องการเปลี่ยนจากการเปิดใช้งานกล้องมาเป็นให้เลือกรูปจากแกลเลอรี่แทนก็ให้ใช้
+//        imagePicker.sourceType = .photoLibrary
+//        แต่ว่าตัว .photoLibrary มันจะถูกลบออกแล้วใน ios16 แหละนะ
+        
+        
+//        ตัวนี้จะอนุญาติให้เราสามารถที่จะแก้ไขรูปภาพ หรือเลือกรูปภาพได้
+        imagePicker.allowsEditing = false
+//        false คือไม่อนุญาติแหละนะ
+    }
+    
+//    ฟังก์ชันนี้มาจากตัว delegate ของ pickerController
+//    หน้าที่ของมันคือหลังจากที่ user เขากดเลือกรูปมาแล้วว่าจะเอารูปนี้ จะให้ทำอะไรต่อ
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        /*
+         มาอธิบาย params กันก่อน
+         picker ก็คือตัว UIImagePickerController ที่ใช้สำหรับเลือกรูปภาพที่เราต้องการนั่นแหละ
+         info ก็คือ ข้อมูลต่าง ๆ ของรูปภาพหรืออะไรก็ตามที่ user กดเข้ามา
+         */
+        
+//        ให้ตัวแปร image มันเก็บรูปภาพที่ผู้ใช้เขาถ่ายมานั่นแหละนะ
+//        ตัว info จะเป็นตัวแปรที่เป็น Dictionary แหละ
+        if let userPickImage = info[.originalImage] as? UIImage {
+            imageView.image = userPickImage
+        }
+        
+//        ก็คือพอเรากดเลือกรูปนี้มาแล้วใช่ป่ะ ก็ให้มันปิดหน้าจอถ่ายรูปออกไปเลย
+        imagePicker.dismiss(animated: true)
     }
 
     @IBAction func cameraTab(_ sender: UIBarButtonItem) {
+        
+//        เมื่อเขากดปุ่มถ่ายรูปมาใช่ป่ะ ก็ให้เปิดใช้งานกล้อง
+        present(imagePicker, animated: true, completion: nil)
         
     }
     
