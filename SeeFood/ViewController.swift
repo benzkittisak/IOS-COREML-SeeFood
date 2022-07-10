55d09e03faa2dc67883867cca808db065c8cac03
benzkittisak <benzkittisak@stv.ac.th>
2022-07-10 19:16:35 +0700

    เรียกใช้ coreML ให้ประมวลผลและวิเคราะห์รูปภาพ

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,7 +6,7 @@
 //
 
 /*
-  คือมันจะพังเรื่อง NSCameraUsageDescription เพราะว่าเราไม่ได้อธิบายเอาไว้ว่าเราใช้กล้องเพราะอะไร
+ คือมันจะพังเรื่อง NSCameraUsageDescription เพราะว่าเราไม่ได้อธิบายเอาไว้ว่าเราใช้กล้องเพราะอะไร
  ดังนั้นเราต้องไปเขียนใน info.plist ก่อน ก็คือไปเพิ่มตัว Privacy เรื่องกล้องนั่นแหละ
  เพิ่ม
  Privacy - Photo Library Usage Description
@@ -25,7 +25,7 @@ import Vision
  */
 
 class ViewController: UIViewController , UIImagePickerControllerDelegate , UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     
     let imagePicker = UIImagePickerController()
@@ -35,20 +35,20 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate , UINav
         
         imagePicker.delegate = self
         
-//        มันจะอนุญาตให้เราสามารถเชื่อมต่อเข้ากลับกล้องได้
+        //        มันจะอนุญาตให้เราสามารถเชื่อมต่อเข้ากลับกล้องได้
         imagePicker.sourceType = .camera
-//        ถ้าต้องการเปลี่ยนจากการเปิดใช้งานกล้องมาเป็นให้เลือกรูปจากแกลเลอรี่แทนก็ให้ใช้
-//        imagePicker.sourceType = .photoLibrary
-//        แต่ว่าตัว .photoLibrary มันจะถูกลบออกแล้วใน ios16 แหละนะ
+        //        ถ้าต้องการเปลี่ยนจากการเปิดใช้งานกล้องมาเป็นให้เลือกรูปจากแกลเลอรี่แทนก็ให้ใช้
+        //        imagePicker.sourceType = .photoLibrary
+        //        แต่ว่าตัว .photoLibrary มันจะถูกลบออกแล้วใน ios16 แหละนะ
         
         
-//        ตัวนี้จะอนุญาติให้เราสามารถที่จะแก้ไขรูปภาพ หรือเลือกรูปภาพได้
+        //        ตัวนี้จะอนุญาติให้เราสามารถที่จะแก้ไขรูปภาพ หรือเลือกรูปภาพได้
         imagePicker.allowsEditing = false
-//        false คือไม่อนุญาติแหละนะ
+        //        false คือไม่อนุญาติแหละนะ
     }
     
-//    ฟังก์ชันนี้มาจากตัว delegate ของ pickerController
-//    หน้าที่ของมันคือหลังจากที่ user เขากดเลือกรูปมาแล้วว่าจะเอารูปนี้ จะให้ทำอะไรต่อ
+    //    ฟังก์ชันนี้มาจากตัว delegate ของ pickerController
+    //    หน้าที่ของมันคือหลังจากที่ user เขากดเลือกรูปมาแล้วว่าจะเอารูปนี้ จะให้ทำอะไรต่อ
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         /*
          มาอธิบาย params กันก่อน
@@ -56,22 +56,56 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate , UINav
          info ก็คือ ข้อมูลต่าง ๆ ของรูปภาพหรืออะไรก็ตามที่ user กดเข้ามา
          */
         
-//        ให้ตัวแปร image มันเก็บรูปภาพที่ผู้ใช้เขาถ่ายมานั่นแหละนะ
-//        ตัว info จะเป็นตัวแปรที่เป็น Dictionary แหละ
+        //        ให้ตัวแปร image มันเก็บรูปภาพที่ผู้ใช้เขาถ่ายมานั่นแหละนะ
+        //        ตัว info จะเป็นตัวแปรที่เป็น Dictionary แหละ
         if let userPickImage = info[.originalImage] as? UIImage {
             imageView.image = userPickImage
+            
+            //            ต่อไปเราจะทำการแปลง UIImage ให้ไปเป็น CIImage เพื่อให้ตัว Vision สามารถใช้งานรูปภาพแล้วให้ CoreML มันเอาไปใช้ได้ด้วย
+            guard let ciimage = CIImage(image: userPickImage) else {
+                fatalError("ไม่สามารถแปลงชนิดของข้อมูลไปเป็น CIImage ได้")
+            }
+            
+            detect(image: ciimage)
+            
+            
         }
         
-//        ก็คือพอเรากดเลือกรูปนี้มาแล้วใช่ป่ะ ก็ให้มันปิดหน้าจอถ่ายรูปออกไปเลย
+        //        ก็คือพอเรากดเลือกรูปนี้มาแล้วใช่ป่ะ ก็ให้มันปิดหน้าจอถ่ายรูปออกไปเลย
         imagePicker.dismiss(animated: true)
     }
-
+    
+    func detect(image:CIImage){
+        //        เรียกใช้งานตัว model ของ Inceptionv3
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else { fatalError("ไม่สามารถเรียกใช้งาน CoreML Model ได้")}
+        
+        //        ขอให้ตัว coreML ช่วยเอารูปของเราไปประมวลผลให้หน่อย
+        let request = VNCoreMLRequest(model: model) { request, error in
+            //        เมื่อ request สามารถทำงานได้สำเร็จ
+            guard let results = request.results as? [VNClassificationObservation] else { fatalError("ไม่สามารถวิเคราะห์ข้อมูลได้") }
+            
+            print(results)
+            
+            
+        }
+        //            ต่อไปถ้ามันประมวลผลรูปสำเร็จ ก็็ให้ตัว Vision เอารูปไปวิเคราะห์ข้อมูล
+        let handler = VNImageRequestHandler(ciImage:image)
+        
+        do {
+            try handler.perform([request])
+        } catch let error {
+            print("ไม่สามารถวิเคราะห์ข้อมูลได้เนื่องจาก \(error)")
+        }
+    }
+    
     @IBAction func cameraTab(_ sender: UIBarButtonItem) {
         
-//        เมื่อเขากดปุ่มถ่ายรูปมาใช่ป่ะ ก็ให้เปิดใช้งานกล้อง
+        //        เมื่อเขากดปุ่มถ่ายรูปมาใช่ป่ะ ก็ให้เปิดใช้งานกล้อง
         present(imagePicker, animated: true, completion: nil)
         
     }
+    
+    
     
 }
 
